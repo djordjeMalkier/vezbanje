f6e57d581071ec2c78afddf358cbb967129abf12
NikolaGardas <nikolagardas0@gmail.com>
2024-09-25 11:13:37 +0200

    Uklonjena HelloWorld jer vise ne treba

--- src/main/java/source/controller/HelloWorldController.java
+++ src/main/java/source/controller/HelloWorldController.java
@@ -6,11 +6,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloWorldController {
 
-    @GetMapping("/hello")
-    public String sayHello() {
-        return "Hello World";
-    }
-
     @GetMapping("/zdravo")
     public String reciZdravo() {
         return "Zdravo Nikola";
